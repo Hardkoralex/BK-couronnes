3d514bc50122d5b1f07304bcaa3e427614734504
Hardkoralex <lordmanson_666@hotmail.com>
2026-09-16 11:43:47 +0200

    Update players.js

--- players.js
+++ players.js
@@ -7,6 +7,7 @@
 const PLAYERS = [
   { name: "Alexandre", crowns: 4, dates: "09/02/2026, 25/07/2026 x2, 04/08/2026" },
   { name: "Michaël",   crowns: 3, dates: "01/12/2025, 29/06/2026, 07/2026" },
+  { name: "Jonathan",   crowns: 2, dates: "10/09/2026, 16/09/2026" },
 ];
 
 const DEPUIS = "décembre 2025";
